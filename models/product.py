1976bf1b78e4d9e12ae4db47942e61b8e7ab1fdd
wt <39181819@qq.com>
2015-10-13 22:57:56 +0800

    bug fix

--- models/product.py
+++ models/product.py
@@ -9,4 +9,4 @@ class Product(models.Model):
     def _check_name(self):
         count = self.search_count([("name", "=", self.name)])
         if count > 1:
-            raise exceptions.ValidationError(_("Product %s already exist!" % self.name))
+            raise exceptions.ValidationError(_("Product %s already exist!") % self.name)
