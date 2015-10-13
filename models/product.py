223d74afb51c772e42c89397b3989cf4444cd925
wt <39181819@qq.com>
2015-10-13 23:05:54 +0800

    加入default_code的判断。
    jason说这更符合实际。

--- models/product.py
+++ models/product.py
@@ -5,8 +5,9 @@ class Product(models.Model):
     _inherit = "product.template"
 
     @api.one
-    @api.constrains('name')
+    @api.constrains('name', 'default_code')
     def _check_name(self):
-        count = self.search_count([("name", "=", self.name)])
+        count = self.search_count([("name", "=", self.name), ("default_code", "=", self.default_code)])
         if count > 1:
-            raise exceptions.ValidationError(_("Product %s already exist!") % self.name)
+            raise exceptions.ValidationError(
+                _("Product %s with default_code %s already exist!") % (self.name, self.default_code))
